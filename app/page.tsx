0fb50b74cc144986753f299ddc9ba9c45b4ebd41
koddaks <koss199710@gmail.com>
2024-07-13 16:50:41 +0300

    Buttons library

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <div>
-      <Button size="lg" variant="destructive">Hello Lingo!</Button>
+      <Button size="lg" >Hello Lingo!</Button>
     </div>
   );
 }
