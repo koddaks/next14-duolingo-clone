452d0cb27dde0c175d8bd1e1e46406ab5feda315
koddaks <koss199710@gmail.com>
2024-07-22 16:32:48 +0300

    Lesson Button

--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,7 @@
 import { cache } from 'react';
 import db from '@/db/drizzle';
 import { auth } from '@clerk/nextjs/server';
-import { courses, units, userProgress } from './schema';
+import { challengeProgress, courses, units, userProgress } from './schema';
 import { eq } from 'drizzle-orm';
 
 export const getUserProgress = cache(async () => {
@@ -22,12 +22,14 @@ export const getUserProgress = cache(async () => {
 });
 
 export const getUnits = cache(async () => {
+  const { userId } = await auth();
   const userProgress = await getUserProgress();
 
-  if (!userProgress?.activeCourseId) {
+  if (!userId || !userProgress?.activeCourseId) {
     return [];
   }
 
+  // TODO: Confirm where order is needed 
   const data = await db.query.units.findMany({
     where: eq(units.courseId, userProgress.activeCourseId),
     with: {
@@ -35,7 +37,9 @@ export const getUnits = cache(async () => {
         with: {
           challenges: {
             with: {
-              challengeProgress: true,
+              challengeProgress: {
+                where: eq(challengeProgress.userId, userId),
+              },
             },
           },
         },
