1e632d3cd637598f5307e105cb841726f21e73c8
koddaks <koss199710@gmail.com>
2024-07-23 12:17:36 +0300

    Course Progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,20 +2,35 @@ import { FeedWrapper } from '@/components/feed-wrapper';
 import { StickyWrapper } from '@/components/sticky-wrapper';
 import { Header } from './header';
 import { UserProgress } from '@/components/user-progress';
-import { getUnits, getUserProgress } from '@/db/queries';
+import {
+  getCourseProgress,
+  getLessonPercentage,
+  getUnits,
+  getUserProgress,
+} from '@/db/queries';
 import { redirect } from 'next/navigation';
 import Unit from './unit';
 
 const LearnPage = async () => {
   const userProgressData = getUserProgress();
   const unitsData = getUnits();
+  const courseProgressData = getCourseProgress();
 
-  const [userProgress, units] = await Promise.all([
-    userProgressData,
-    unitsData,
-  ]);
+  const lessonPercentageData = getLessonPercentage();
+
+  const [userProgress, units, courseProgress, lessonPercentage] =
+    await Promise.all([
+      userProgressData,
+      unitsData,
+      courseProgressData,
+      lessonPercentageData,
+    ]);
 
   if (!userProgress || !userProgress.activeCourse) {
+    redirect('/courses');
+  }
+
+  if (!courseProgress) {
     redirect('/courses');
   }
 
@@ -39,8 +54,8 @@ const LearnPage = async () => {
               title={unit.title}
               lessons={unit.lessons}
               description={unit.description}
-              activeLesson={undefined}
-              activeLessonPercentage={0}
+              activeLesson={courseProgress.activeLesson}
+              activeLessonPercentage={lessonPercentage}
             />
           </div>
         ))}
