5428d216926563ed0fb2f3da08db23faf2f4cd64
koddaks <koss199710@gmail.com>
2024-07-24 15:36:40 +0300

    Challenge Footer

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -5,6 +5,8 @@ import { useState } from 'react';
 import Header from './header';
 import { QuestionBubble } from './question-bubble';
 import { Challenge } from './challenge';
+import { stat } from 'fs';
+import { Footer } from './footer';
 
 type QuizProps = {
   initialPercentage: number;
@@ -34,8 +36,17 @@ const Quiz = ({
     return uncompletedIndex === -1 ? 0 : uncompletedIndex;
   });
 
+  const [selectedOption, setSelectedOption] = useState<number>();
+  const [status, setStatus] = useState<'correct' | 'wrong' | 'none'>('none');
+
   const challenge = challenges[activeIndex];
   const options = challenge?.challengeOptions ?? [];
+
+  const onSelect = (id: number) => {
+    if (status !== 'none') return;
+
+    setSelectedOption(id);
+  };
 
   const title =
     challenge.type === 'ASSIST'
@@ -56,21 +67,22 @@ const Quiz = ({
               {title}
             </h1>
             <div>
-              {challenge.type === "ASSIST" && (
+              {challenge.type === 'ASSIST' && (
                 <QuestionBubble question={challenge.question} />
               )}
               <Challenge
-              options={options}
-              onSelect={() => {}}
-              status="none"
-              selectedOption={undefined}
-              disabled={false}
-              type={challenge.type}
+                options={options}
+                onSelect={onSelect}
+                status={status}
+                selectedOption={selectedOption}
+                disabled={false}
+                type={challenge.type}
               />
             </div>
           </div>
         </div>
       </div>
+      <Footer disabled={!selectedOption} status={status} onCheck={() => {}} />
     </>
   );
 };
