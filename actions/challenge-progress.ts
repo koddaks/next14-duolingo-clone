e0f8250b41c6c36891d28dbc056a915baba351ba
koddaks <koss199710@gmail.com>
2024-08-12 17:26:09 +0300

    fix upsertChallengeProgress  revalidatePath

--- actions/challenge-progress.ts
+++ actions/challenge-progress.ts
@@ -84,4 +84,10 @@ export const upsertChallengeProgress = async (challengeId: number) => {
       points: currentUserProgress.points + 10,
     })
     .where(eq(userProgress.userId, userId));
+
+  revalidatePath('/learn');
+  revalidatePath('/lesson');
+  revalidatePath('/quests');
+  revalidatePath('/leaderboard');
+  revalidatePath(`/lesson/${lessonId}`);
 };
